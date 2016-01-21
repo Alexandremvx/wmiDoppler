c7f4e3d5f1dff74acb82a96995a0f1aae62a9bf0
Alexandremvx <alexandre.francisco@outlook.com.br>
2016-01-21 14:05:49 -0200

    Update wmi-doppler.ps1

--- wmi-doppler.ps1
+++ wmi-doppler.ps1
@@ -1,53 +1,45 @@
-<#
-Wmi-Doppler = sends wmi command accross the network using remote sessions when needed to reach the target machine
-list jumps
-create session
-    check/alter credentials (if err)
-list targets
-check if can reach without jump (wmi)
-    check jumps acess target (if err)
-runs wmi command and store exit
-#>
+## GENERIC FUNCTIONS ##
+function promptSN($msg,$def=0) {
+ $titulo = "Wmi-Doppler"
+ $sim = New-Object System.Management.Automation.Host.ChoiceDescription "&Sim", "Sim"
+ $nao = New-Object System.Management.Automation.Host.ChoiceDescription "&Não", "Não"
+ $options = [System.Management.Automation.Host.ChoiceDescription[]]($nao,$sim)
+ $result = $host.ui.PromptForChoice($titulo, $msg, $options,$def) 
+ switch ($result){
+  1 {return $true}
+  0 {return $false}
+ }
+}
 
-#if (-not $PSScriptRoot) {$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition}
+function read-list{param([parameter(mandatory=$true)][String[]]${@})if(${@}){return ${@}}}
 
-## GLOBAL VARIABLES ##
-$ErrorActionPreference = "silentlycontinue"
-
-
-## LOAD FUNCTIONS LIB ##
-. .\lib-doppler.ps1
-
-
-
-Write-Warning " # # # load-auth # # # "
-$wAuth = Load-AuthCsv
-
-
-Write-Warning " # # # list jumps # # # "
-$wJumps = Load-JumperFile
-
-Write-Warning " # # # create session # # #"
-$wSessions = create-JumpSessions $wJumps $wAuth
-
-
-Invoke-Command -Session $wSessions {hostname}
-
-Write-Warning "    check/alter credentials (if err)"
-Write-Warning "list targets"
-Write-Warning "check if can reach without jump (wmi)"
-Write-Warning "   check jumps acess target (if err)"
-Write-Warning "runs wmi command and store exit"
+Function TimedPrompt($prompt,$secondsToWait){	
+#Source:https://gist.github.com/nathanchere/704920a4a43f06f4f0d2
+	Write-Host -NoNewline $prompt
+	$secondsCounter = 0
+	$subCounter = 0
+	While ( (!$host.ui.rawui.KeyAvailable) -and ($count -lt $secondsToWait) ){
+		start-sleep -m 10
+		$subCounter = $subCounter + 10
+		if($subCounter -eq 1000)
+		{
+			$secondsCounter++
+			$subCounter = 0
+			Write-Host -NoNewline "."
+		}		
+		If ($secondsCounter -eq $secondsToWait) { 
+			Write-Host "`r`n"
+			return $false;
+		}
+	}
+	Write-Host "`r`n"
+	return $true;
+}
 
 
-
-
-
-Write-Warning "teste area"
+## AUTH MANAGEMENT FUNCTIONS ##
 function Read-AuthList(){
- #http://blogs.msdn.com/b/koteshb/archive/2010/02/13/powershell-creating-a-pscredential-object.aspx
  $creds = @()
- 
  do{
   $cUser = Read-Host -Prompt "User"
   if ($cUser) {
@@ -55,11 +47,77 @@ function Read-AuthList(){
    $creds += New-Object System.Management.Automation.PSCredential ($cUser,$cPass)
   }
  }while($cUser)
-
  if ($creds.length -eq 0) {
   Write-Warning "Nenhuma credencial fornecida"
   return $null
  }else{
   return $creds
+ }
+}
+
+
+function Save-AuthCsv($Auth, $csvFile="Credentials.csv"){
+ $Auth.getnetworkcredential() |
+  select Username,Domain,@{N="SecurePassword";E={$_.securepassword | ConvertFrom-SecureString -Key (16..31)}} |
+   ConvertTo-Csv -Delimiter ";" -NoTypeInformation |
+    Out-File -FilePath $csvFile -Append 
+}
+
+function Load-AuthCsv($csvFile="Credentials.csv"){
+ if (-not $(ls $csvFile)) { Write-Warning "Arquivo não encontrado $csvFile"; return $null}
+ $csvPass = cat $csvFile | Convertfrom-Csv -Delimiter ";"
+ $csvAuth = $csvPass | % { New-Object System.Management.Automation.PSCredential ("$($_.domain)\$($_.Username)",$($_.SecurePassword | ConvertTo-SecureString -Key (16..31))) }
+ return $csvAuth
+}
+
+
+## JUMPER LIST MANAGEMENT FUNCTIONS ##
+function Read-JumperList(){
+ $jumps = @()
+ do{
+  $jump = Read-Host -Prompt "Jumper"
+  if ($jump) {    $jumps += $jump   }
+ }while($jump)
+ if ($jumps.length -eq 0) {
+  Write-Warning "Nenhum servidor fornecido"
+  return $null
+ }else{
+  return $jumps
+ }
+}
+
+function Save-JumperFile($Jumps, $jumpFile="jumps.txt"){
+ $Jumps | Out-File -FilePath $jumpFile -Append
+}
+
+function Load-JumperFile($jumpFile="jumps.txt"){
+ #$jumps = @()
+ if (-not $(ls $jumpFile)) { Write-Warning "Arquivo não encontrado $jumpFile"; return $null}
+ $jumps = cat $jumpFile
+ return $jumps
+}
+
+## JUMPER SESSIONS FUNCTIONS ##
+function create-JumpSessions($Jumps,$Auth){
+ if (-not $Jumps -and -not $Auth) {Write-Warning 'create-JumpSessions <JumpList> <AuthList>'; return $null}
+ $Sess = @()
+ foreach ($j in $Jumps) {
+  foreach ($a in $auth) {
+   $s = New-PSSession -Name "$($a.username)@$j" -ComputerName $j -Credential $a -ErrorVariable sErr
+   if ($s) {
+    $Sess += $s
+    break
+   }elseif($($sErr[0].Exception.ErrorCode) -ne 5){
+    Write-Warning "Servidor de Jump inacessivel [$j]"
+    break
+   }
+  }
+  if (-not $s -and $($sErr[0].Exception.ErrorCode) -eq 5) {Write-Warning "Servidor de Jump sem credenciais válidas [$j]"}
+ }
+ if ($Sess.length -eq 0) {
+  Write-Warning "Nenhuma sessão criada."
+  return $null
+ }else{
+  return $Sess
  }
 }
