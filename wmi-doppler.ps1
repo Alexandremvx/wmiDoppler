c64aa5977dfc1f852658ca4698bea48c0fbfd6ee
Alexandremvx <alexandre.francisco@outlook.com.br>
2016-02-10 17:43:20 -0200

    Update wmi-doppler.ps1

--- wmi-doppler.ps1
+++ wmi-doppler.ps1
@@ -12,12 +12,11 @@ runs wmi command and store exit
 #if (-not $PSScriptRoot) {$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition}
 
 ## GLOBAL VARIABLES ##
-$ErrorActionPreference = "Continue"#"silentlycontinue"
 $ErrorActionPreference = "Silentlycontinue"
+$logfile = ".\wmi_log.log"
 
 ## LOAD FUNCTIONS LIB ##
 . .\lib-doppler.ps1 #Caminho do arquivos de funções
-
 
 
 Write-Warning " # # # load-files # # # "
@@ -35,42 +34,35 @@ Write-Warning " # # # check target connectivity and call job by session # # # "
  
  $wJobs = @()
 
- foreach ($target in $wTargets) {
-  
-  $targetJump = find-TargetJump $target $wSessions
- 
-  if ($targetJump) { echo "@_ $target online via $($tj.computername)"} else { echo "X_ $target não acessivel"}
 
-  if ($targetJump) {
-   $wJobs += Invoke-Command -Session $targetJump -ArgumentList $target,$wAuth -FilePath .\HV4.ps1 -AsJob -JobName $target
-
-  }
-
- }
-
-
-# Receive-Job $wJobs -Wait
-
-return "noop"
-
-
-foreach ($t in $wTarget) {
- $TargetJob = @{Auth = $wAuth;Sessions=$wSessions;tName=$t}
- $wJobs += Start-Job -Name $t -ArgumentList $TargetJob -ScriptBlock {
-  $wAuth = $args[0].Auth;$wSessions=$args[0].Sessions;$t=$args[0].tName
-  foreach ($ws in $wSessions){
-   $ws
-   $ping = new-object system.net.networkinformation.ping
-   $ping.Send($t)
-  
-  
-  }
-
-
- }
+$reportFile = "report.csv"
+if (-not (ls $reportFile -ErrorAction SilentlyContinue)) {
+ Write-Output "HOSTNAME;CONFIGURAÇÃO HV;TIME ZONE;DLBIAS;DATA ATUAL;HORA ATUAL;DIFERENÇA EM MINUTOS;INÍCIO HV;TERMINHO HV" | Out-File $reportFile
 }
 
 
-Write-Warning "   check jumps acess target (if err)"
-Write-Warning "runs wmi command and store exit"
+ foreach ($target in $wTargets) {
+  $targetJump = find-TargetJump $target $wSessions
+  #if ((($wJobs | ? State -eq "Running").count) -ge 20){sleep -Milliseconds 500}
+  $msg = if ($targetJump) { "@_ $target online via $($targetJump.computername)"} else { "X_ $target não acessivel"}
+  $msg | Out-File -FilePath $logfile -Append
+  echo $msg
+  if (-not $targetJump) { "$target" | Out-File -FilePath ".\err_hosts.txt" -Append}
+  if ($targetJump) {
+   #$wJobs += Invoke-Command -Session $targetJump -ArgumentList $target,$wAuth -FilePath .\HV4.ps1 #-AsJob -JobName $target
+            Invoke-Command -Session $targetJump -ArgumentList $target,$wAuth -FilePath .\HV4.ps1 | % {$_ | Out-File $reportFile -Append}
+   #Invoke-Command -Session $targetJump -ArgumentList $target,$wAuth -FilePath .\HV4.ps1
+  }
+ }
+
+#if ((($wJobs | ? State -eq "Running").count) -ge 1){sleep -Milliseconds 500}
+
+Write-Warning " # # # wait jobs finish and save report # # # "
+#$wJobs #| Out-File $reportFile -Append
+
+ #if ((($wJobs | ? State -eq "Running").count) -gt 0){sleep -Milliseconds 500}
+
+# Receive-Job $wJobs | Receive-Job #| Out-File -FilePath "WMI_report.csv"
+
+#return $null
 
